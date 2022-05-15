af84d2f0628636cd657c22ef313781cd8a613838
Jesus Valenzuela <jesus_valenzuelag25@gmail.com>
2022-05-15 15:06:17 -0600

    se sube repo inicial

---
+++ Timbrado_Enlaces/src/main/java/com/example/Timbrado_Enlaces/TimbradoEnlacesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Timbrado_Enlaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimbradoEnlacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimbradoEnlacesApplication.class, args);
+	}
+
+}
